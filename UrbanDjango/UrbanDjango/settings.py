a1e2894ae8c54eca6c6e0e8b7771ba73e2e65129
ZBook <izuraeva@gmail.com>
2024-12-29 01:21:11 +0300

    mod_18_ex_2

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example1'
+    'task2',
 ]
 
 MIDDLEWARE = [
--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def func_templ(request):
+    return render(request, 'second_task/func_template.html')
+
+def class_templ(request):
+    return render(request, 'second_task/class_template.html')
